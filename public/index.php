894079cefaa8b15147160b4c722fbb6af9423f60
James Sosontovich <jjpsos@videotron.ca>
2024-03-06 10:53:16 -0500

    Code Example

--- public/index.php
+++ public/index.php
@@ -4,10 +4,9 @@ use JimSos\Framework\Http\Request;
 use JimSos\Framework\Http\Response;
 use JimSos\Framework\Http\Kernel;
 
-// public/index.php
 /**
 *  PHP Framework for Web Applications
-*  Step by step by numbered steps (#)   
+*  Request -> Response Cycle   
 */
 
 // Debugging 
@@ -18,23 +17,10 @@ require_once $rootPath . '/vendor/autoload.php';
 
 // request received (1)
 $request = Request::createFromGlobals();
-//dd($request);
-
-// perform some logic
-// send response (string of content) (2)
-// $content = '<h1>RAMP Framework</h1>';
-// $response = new Response(content: $content, status: 200, headers: []);
-// $response->send();
 
 // perform some logic (3)
 $kernel = new Kernel();
+
 // send response (string of content)
 $response = $kernel->handle($request);
-/* dd($response);
-JimSos\Framework\Http\Response {#7 ▼
-    -content: "<h1>RAMP KERNEL</h1>"
-    -status: 200
-    -headers: []
-  }
-*/
 $response->send();
