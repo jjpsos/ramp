21860708aa0a580fcd7c11827261c678b72d79f4
James Sosontovich <jjpsos@videotron.ca>
2024-07-16 17:14:26 -0400

    Safe Config

--- store/app/config/config.php
+++ store/app/config/config.php
@@ -10,10 +10,10 @@ defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 return new \Phalcon\Config\Config([
     'database' => [
         'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
+        'host'        => '127.0.0.1',
+        'username'    => '',
         'password'    => '',
-        'dbname'      => 'test',
+        'dbname'      => 'classicpress',
         'charset'     => 'utf8',
     ],
     'application' => [
